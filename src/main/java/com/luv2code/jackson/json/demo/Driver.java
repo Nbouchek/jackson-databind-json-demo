5bf715da9b122ba1d64d5f00fe063b1cab27e99c
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-16 09:36:41 +0100

    How to use jackson with spring

---
+++ src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -0,0 +1,27 @@
+package com.luv2code.jackson.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+    public static void main(String[] args) {
+        try {
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // read JSON file and map/convert to JAVA Pojo
+            // data sample-lite.json
+            Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+            // print first name and last name
+            System.out.println("id = " + theStudent.getId());
+            System.out.println("First name = " + theStudent.getFirstName());
+            System.out.println("Last name = " + theStudent.getLastName());
+            System.out.println("iSActive = " + theStudent.isActive());
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -0,0 +1,40 @@
+package com.luv2code.jackson.json.demo;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean isActive;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
